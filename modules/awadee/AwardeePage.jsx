970104ac6b4980a84c9422241cf07e96e6a111e0
joko19 <jokoslamet2207@gmail.com>
2023-02-20 03:44:16 +0700

    awardee card

--- modules/awadee/AwardeePage.jsx
+++ modules/awadee/AwardeePage.jsx
@@ -1,65 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
-import Link from "next/link";
-import { getDatabase } from "../../lib/notion";
-// import styles from "./index.module.css";
 import Header from "../../components/Header";
 import Image from "next/image";
-import clsx from "clsx";
-import { FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { useState } from "react";
 import ReactDropdown from "react-dropdown";
+import { AwardeeCard } from "./components/AwardeeCard";
 
-// export const databaseId = '535d3dbfb89c4069bb1d8392e3a03976';
-const AwardeeCard = ({ data }) => {
-  return (
-    <div
-      className={clsx(
-        "w-full lg:ml-4 lg:mr-4 lg:mb-[80px] relative",
-        " ml-0 mr-3 mb-[60px]"
-      )}
-    >
-      <img
-        alt={data.properties.name.title[0].plain_text}
-        src={data.properties.image.files[0].file.url}
-        width={100}
-        height={100}
-        className={clsx(
-          "lg:w-[7vw] lg:h-[7vw] absolute lg:-top-[3.5vw] left-5 rounded-full",
-          "w-[15vw] h-[15vw] -top-[8vw]"
-        )}
-      />
-      <div
-        className={clsx(
-          "bg-gray-100 rounded min-h-[180px] lg:p-5 shadow-[0_16px_20px_rgba(154,170,207,0.25)]",
-          "pt-2 pr-3 pb-3.5 pl-5"
-        )}
-      >
-        {" "}
-        <div className="h-14" />
-        <div
-          className={clsx(
-            "font-semibold lg:text-xl lg:my-0",
-            "mt-2 mb-1.5 text-base"
-          )}
-        >
-          {data.properties.name.title[0].plain_text}
-        </div>
-        <div className="lg:text-md sm-only:text-sm">
-          {" "}
-          {data.properties.university.rich_text[0].plain_text}
-        </div>
-      </div>
-    </div>
-  );
-};
+function compare(a, b) {
+  if (
+    a.properties.name.title[0].plain_text <
+    b.properties.name.title[0].plain_text
+  ) {
+    return -1;
+  }
+  if (
+    a.properties.name.title[0].plain_text >
+    b.properties.name.title[0].plain_text
+  ) {
+    return 1;
+  }
+  return 0;
+}
 
 export default function AwardeePage({ posts, filter }) {
-  const [awardeeList, setAwardeeList] = useState(posts);
-  // console.log("hello world")
-  // console.log(posts[0].properties)
-  console.log(posts);
-  // console.log(posts[0].properties.image.files[0].file.url);
+  const [awardeeList, setAwardeeList] = useState(posts.sort(compare));
 
   const handleSearch = (e) => {
     let awardeeTemp = posts;
@@ -103,7 +67,7 @@ export default function AwardeePage({ posts, filter }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className="pt-64 px-12">
+      <main className="px-12 pt-32">
         <div className="flex flex-col sm:flex-row gap-4">
           <input
             placeholder="Cari Awardee"
@@ -117,18 +81,10 @@ export default function AwardeePage({ posts, filter }) {
             value="Pilih Program"
             placeholder="Select an option"
           />
-          {/* <select name="cars" className="w-1/4 rounded p-2 focus:outline-none cursor-pointer" placeholder="Program">
-            <option disabled>Program</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
-          </select> */}
         </div>
-
+        {/* {awardeeList.length > 0 && <div>{awardeeList.length} awardee</div>} */}
         <div className="h-24" />
-        <div className="grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-1 grid-flow-row lg:gap-12 gap-4 ">
-          {/* <div className={clsx("relative")}> */}
-          {/* <div className="flex flex-wrap lg:pl-0 pl-4"> */}
+        <div className="grid 2xl:grid-cols-4 sm:grid-cols-3 grid-cols-1 grid-flow-row lg:gap-12 gap-4 ">
           {awardeeList.map((post) => (
             <AwardeeCard data={post} key={post.id} />
           ))}
@@ -141,7 +97,9 @@ export default function AwardeePage({ posts, filter }) {
               width={350}
               alt="logo"
             />
-            <h1 className="mt-4 text-2xl text-red-500">Awardee yang anda cari tidak ditemukan</h1>
+            <h1 className="mt-4 text-2xl text-red-500">
+              Awardee yang anda cari tidak ditemukan
+            </h1>
           </div>
         )}
       </main>
---
+++ modules/awadee/components/AwardeeCard.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable @next/next/no-img-element */
+import clsx from "clsx";
+import {
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+  FaFacebookSquare,
+} from "react-icons/fa";
+
+export const AwardeeCard = ({ data }) => {
+  return (
+    <div
+      className={clsx(
+        "w-full lg:ml-4 lg:mr-4 lg:mb-[80px] relative",
+        " ml-0 mr-3 mb-[60px]"
+      )}
+    >
+      {data.properties.image.files.length > 0 ? (
+        <img
+          alt={data.properties.name.title[0].plain_text}
+          src={data.properties.image.files[0].file.url}
+          width={100}
+          height={100}
+          className={clsx(
+            "lg:w-[7vw] lg:h-[7vw] absolute lg:-top-[3.5vw] left-5 rounded-full",
+            "w-[15vw] h-[15vw] -top-[8vw]"
+          )}
+        />
+      ) : (
+        <img
+          src="/img/logo.png"
+          width={100}
+          height={100}
+          className={clsx(
+            "lg:w-[7vw] lg:h-[7vw] absolute lg:-top-[3.5vw] left-5 rounded-full",
+            "w-[15vw] h-[15vw] -top-[8vw]"
+          )}
+          alt="logo"
+        />
+      )}
+      <div
+        className={clsx(
+          "bg-gray-100 rounded min-h-[180px] lg:p-5 shadow-[0_16px_20px_rgba(154,170,207,0.25)]",
+          "pt-2 pr-3 pb-3.5 pl-5"
+        )}
+      >
+        <div className="h-14" />
+        <div
+          className={clsx(
+            "font-semibold lg:text-xl lg:my-0",
+            "mt-2 mb-1.5 text-base"
+          )}
+        >
+          {data.properties.name.title[0].plain_text}
+        </div>
+        <div className="lg:text-md sm-only:text-sm">
+          {data.properties.university.rich_text[0].plain_text}
+        </div>
+      </div>
+    </div>
+  );
+};
