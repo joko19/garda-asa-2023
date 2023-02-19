94f59e0aec8b2a886d9ccd4ac910a9ede9efa072
joko19 <jokoslamet2207@gmail.com>
2023-02-19 21:20:58 +0700

    student area

--- modules/Landing/components/Banner.jsx
+++ modules/Landing/components/Banner.jsx
@@ -6,12 +6,19 @@ function Banner() {
     <div className="flex flex-col-reverse sm:flex-row gap-2 justify-between items-center bg-gray-50 sm:pt-32 sm:px-20 pt-24 pb-12">
       <div className="sm:text-left text-center">
         <div className="sm:text-5xl text-2xl text-red-500 items-center">
-          <span className="font-bold text-orange-500 ">
-            Garda ASA
-          </span>{" "}
-          | <span className="text-yellow-500">PK 199</span>
+          <span className="font-bold text-orange-500 ">Garda ASA</span> |{" "}
+          <span className="text-yellow-500">PK 199</span>
         </div>
-        <div className="sm:mt-2 text-gray-500 sm:text-2xl">Bersinergi Membangun Bangsa, Terangi Penjuru Negeri</div>
+        <div className="sm:mt-2 text-gray-500 sm:text-2xl">
+          Gelorakan <span className="text-yellow-500">ASA</span> untuk Indonesia
+          raya!
+        </div>
+        <p className="w-1/2 text-gray-500">
+          Perayaan satu langkah lagi dari kemajuan edukasi bagi putra-putri
+          Indonesia. Selebrasi luhurnya visi dan mimpi, diekspresikan dengan
+          aksi menyebarkan semangat perjuangan bagi antar Awardee maupun
+          masyarakat demi kemajuan bangsa.
+        </p>
       </div>
 
       <Image src="/img/logo.png" height={350} width={350} alt="logo" />
--- modules/Landing/components/StudentArea.jsx
+++ modules/Landing/components/StudentArea.jsx
@@ -3,11 +3,18 @@ import Image from "next/image";
 
 function StudentArea() {
   return (
-    <div className="sm:px-20 text-center">
-        <div className="text-gray-500 text-xl font-medium mt-12">
-            Daerah Asal Penerima Beasiswa
-        </div>
+    <div className="text-center">
+      <div className="text-gray-500 text-xl font-medium mt-12">
+        Peta Persebaran Penerima Beasiswa
+      </div>
       <Image src="/img/indonesia.png" height={450} width={1350} alt="logo" />
+      <div className="bg-orange-300 sm:px-20 py-12 text-gray-600">
+        <div className="sm:text-2xl">
+          “Pendidikan adalah senjata paling mematikan di dunia, karena dengan
+          pendidikan, kamu dapat mengubah dunia”
+        </div>
+        Nelson Mandela
+      </div>
     </div>
   );
 }
